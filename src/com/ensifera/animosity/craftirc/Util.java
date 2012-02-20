051a7da5b57a66cde47765bd2fc51651e2e856a7
Matt Baxter <matt@phozop.net>
2012-02-19 21:02:55 -0500

    combineSplit optimization

--- src/com/ensifera/animosity/craftirc/Util.java
+++ src/com/ensifera/animosity/craftirc/Util.java
@@ -1,18 +1,29 @@
 package com.ensifera.animosity.craftirc;
 
 /**
- * @author Animosity
- *
+ * Utility class
  */
 public class Util {
 
-    // Combine string array with delimiter
+    /**
+     * Combine string array with delimiter
+     * 
+     * @param initialPos
+     * @param parts
+     * @param delimiter
+     * @return
+     * @throws ArrayIndexOutOfBoundsException
+     */
     public static String combineSplit(int initialPos, String[] parts, String delimiter) throws ArrayIndexOutOfBoundsException {
-        if (initialPos >= parts.length) return "";
-        String result = parts[initialPos];
-        for (int i = initialPos + 1; i < parts.length; i++)
-            result = result + delimiter + parts[i];
-        return result;
+        if (initialPos >= parts.length) {
+            return "";
+        }
+        final StringBuilder result = new StringBuilder();
+        result.append(parts[initialPos]);
+        for (int i = initialPos + 1; i < parts.length; i++) {
+            result.append(delimiter + parts[i]);
+        }
+        return result.toString();
     }
-        
+
 }
