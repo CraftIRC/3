ee10e47de3b780a122791dd8723f9a4aa7443b41
Matt Baxter <matt@phozop.net>
2012-03-10 03:37:29 -0500

    IRCCommandSender update and prettify

--- src/com/ensifera/animosity/craftirc/IRCCommandSender.java
+++ src/com/ensifera/animosity/craftirc/IRCCommandSender.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import org.bukkit.Server;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.conversations.Conversation;
+import org.bukkit.conversations.ConversationAbandonedEvent;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionAttachment;
 import org.bukkit.permissions.PermissionAttachmentInfo;
@@ -22,18 +23,53 @@ public class IRCCommandSender implements ConsoleCommandSender {
         this.sender = sender;
     }
 
-    public String getField(String name) {
-        return this.cmd.getField(name);
+    @Override
+    public void abandonConversation(Conversation conversation) {
+        this.sender.abandonConversation(conversation);
     }
 
     @Override
-    public void sendMessage(String message) {
-        try {
-            final RelayedMessage msg = this.cmd.getPlugin().newMsgToTag(this.console, this.cmd.getField("source"), "generic");
-            msg.post();
-        } catch (final Exception e) {
-            e.printStackTrace();
-        }
+    public void abandonConversation(Conversation conversation, ConversationAbandonedEvent details) {
+        this.sender.abandonConversation(conversation, details);
+    }
+
+    @Override
+    public void acceptConversationInput(String input) {
+        this.sender.acceptConversationInput(input);
+    }
+
+    @Override
+    public PermissionAttachment addAttachment(Plugin plugin) {
+        return this.sender.addAttachment(plugin);
+    }
+
+    @Override
+    public PermissionAttachment addAttachment(Plugin plugin, int ticks) {
+        return this.sender.addAttachment(plugin, ticks);
+    }
+
+    @Override
+    public PermissionAttachment addAttachment(Plugin plugin, String name, boolean value) {
+        return this.sender.addAttachment(plugin, name, value);
+    }
+
+    @Override
+    public PermissionAttachment addAttachment(Plugin plugin, String name, boolean value, int ticks) {
+        return this.sender.addAttachment(plugin, name, value, ticks);
+    }
+
+    @Override
+    public boolean beginConversation(Conversation conversation) {
+        return this.sender.beginConversation(conversation);
+    }
+
+    @Override
+    public Set<PermissionAttachmentInfo> getEffectivePermissions() {
+        return this.sender.getEffectivePermissions();
+    }
+
+    public String getField(String name) {
+        return this.cmd.getField(name);
     }
 
     @Override
@@ -47,95 +83,13 @@ public class IRCCommandSender implements ConsoleCommandSender {
     }
 
     @Override
-    public PermissionAttachment addAttachment(Plugin arg0) {
-        return this.sender.addAttachment(arg0);
+    public boolean hasPermission(Permission perm) {
+        return this.sender.hasPermission(perm);
     }
 
     @Override
-    public PermissionAttachment addAttachment(Plugin arg0, int arg1) {
-        return this.sender.addAttachment(arg0, arg1);
-    }
-
-    @Override
-    public PermissionAttachment addAttachment(Plugin arg0, String arg1, boolean arg2) {
-        return this.sender.addAttachment(arg0, arg1, arg2);
-    }
-
-    @Override
-    public PermissionAttachment addAttachment(Plugin arg0, String arg1, boolean arg2, int arg3) {
-        return this.sender.addAttachment(arg0, arg1, arg2, arg3);
-    }
-
-    @Override
-    public Set<PermissionAttachmentInfo> getEffectivePermissions() {
-        return this.sender.getEffectivePermissions();
-    }
-
-    @Override
-    public boolean hasPermission(String arg0) {
-        return this.sender.hasPermission(arg0);
-    }
-
-    @Override
-    public boolean hasPermission(Permission arg0) {
-        return this.sender.hasPermission(arg0);
-    }
-
-    @Override
-    public boolean isPermissionSet(String arg0) {
-        return this.sender.isPermissionSet(arg0);
-    }
-
-    @Override
-    public boolean isPermissionSet(Permission arg0) {
-        return this.sender.isPermissionSet(arg0);
-    }
-
-    @Override
-    public void recalculatePermissions() {
-        this.sender.recalculatePermissions();
-    }
-
-    @Override
-    public void removeAttachment(PermissionAttachment arg0) {
-        this.sender.removeAttachment(arg0);
-    }
-
-    @Override
-    public void setOp(boolean arg0) {
-        this.sender.setOp(arg0);
-    }
-
-    @Override
-    public boolean isOp() {
-        return this.sender.isOp();
-    }
-
-    @Override
-    public void sendMessage(String[] arg0) {
-        try {
-            for (final String message : arg0) {
-                final RelayedMessage msg = this.cmd.getPlugin().newMsgToTag(this.console, this.cmd.getField("source"), "generic");
-                msg.post();
-            }
-        } catch (final Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void abandonConversation(Conversation arg0) {
-        this.sender.abandonConversation(arg0);
-    }
-
-    @Override
-    public void acceptConversationInput(String arg0) {
-        this.sender.acceptConversationInput(arg0);
-    }
-
-    @Override
-    public boolean beginConversation(Conversation arg0) {
-        return this.sender.beginConversation(arg0);
+    public boolean hasPermission(String name) {
+        return this.sender.hasPermission(name);
     }
 
     @Override
@@ -144,12 +98,66 @@ public class IRCCommandSender implements ConsoleCommandSender {
     }
 
     @Override
-    public void sendRawMessage(String message) {
+    public boolean isOp() {
+        return this.sender.isOp();
+    }
+
+    @Override
+    public boolean isPermissionSet(Permission perm) {
+        return this.sender.isPermissionSet(perm);
+    }
+
+    @Override
+    public boolean isPermissionSet(String name) {
+        return this.sender.isPermissionSet(name);
+    }
+
+    @Override
+    public void recalculatePermissions() {
+        this.sender.recalculatePermissions();
+    }
+
+    @Override
+    public void removeAttachment(PermissionAttachment attachment) {
+        this.sender.removeAttachment(attachment);
+    }
+
+    @Override
+    public void sendMessage(String message) {
         try {
             final RelayedMessage msg = this.cmd.getPlugin().newMsgToTag(this.console, this.cmd.getField("source"), "generic");
             msg.post();
         } catch (final Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void sendMessage(String[] messages) {
+        try {
+            for (final String message : messages) {
+                final RelayedMessage msg = this.cmd.getPlugin().newMsgToTag(this.console, this.cmd.getField("source"), "generic");
+                msg.setField("message", message);
+                msg.post();
+            }
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void sendRawMessage(String message) {
+        try {
+            final RelayedMessage msg = this.cmd.getPlugin().newMsgToTag(this.console, this.cmd.getField("source"), "generic");
+            msg.setField("message", message);
+            msg.post();
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void setOp(boolean value) {
+        this.sender.setOp(value);
     }
 }
