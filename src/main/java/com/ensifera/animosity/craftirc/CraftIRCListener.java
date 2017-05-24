4e75dda0c08a0d7fdf1b64c3d3e12c8f13cc1fa9
jacob1 <jfu614@gmail.com>
2017-05-24 01:51:27 +0000

    Replace achievements with advancements

--- src/main/java/com/ensifera/animosity/craftirc/CraftIRCListener.java
+++ src/main/java/com/ensifera/animosity/craftirc/CraftIRCListener.java
@@ -7,7 +7,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.event.player.PlayerAchievementAwardedEvent;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
@@ -173,19 +173,19 @@ final class CraftIRCListener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
-    public void onPlayerAchievement(PlayerAchievementAwardedEvent event) {
-        if (this.plugin.isHeld(CraftIRC.HoldType.ACHIEVEMENTS)) {
+    public void onPlayerAdvancement(PlayerAdvancementDoneEvent event) {
+        if (this.plugin.isHeld(CraftIRC.HoldType.ADVANCEMENTS)) {
             return;
         }
-        if (event.getAchievement() == null) {
+        if (event.getAdvancement() == null) {
             return;
         }
-        final RelayedMessage msg = this.plugin.newMsg(this.plugin.getEndPoint(this.plugin.cMinecraftTag()), null, "achievement");
+        final RelayedMessage msg = this.plugin.newMsg(this.plugin.getEndPoint(this.plugin.cMinecraftTag()), null, "advancement");
         if (msg == null) {
             return;
         }
         msg.setField("sender", event.getPlayer().getDisplayName());
-        msg.setField("message", event.getAchievement().name());
+        msg.setField("message", event.getAdvancement().getKey().getKey());
         msg.setField("world", event.getPlayer().getWorld().getName());
         msg.setField("realSender", event.getPlayer().getName());
         msg.setField("prefix", this.plugin.getPrefix(event.getPlayer()));
